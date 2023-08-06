efe195a68fbc6026b98f4055659684954f14c873
XartyCD <Frexozagsap@yandex.ru>
2023-08-06 12:28:27 +0300

    added JS and optimized info-page size

--- info-page/app.js
+++ info-page/app.js
@@ -1,5 +1,11 @@
+
 const burgerButton = document.getElementById("burger")
 const burgerMenu = document.getElementById("menu")
+const btnsFriends = document.querySelectorAll(".btn-friends-popup")
+const friendsWindow = document.getElementById("popup-friends")
+const closeBtnfriends = document.getElementById("close-btn-friends-popup")
+
+// Бургер меню (знаю что плохо реализовано (пока и так сойдет))
 let burgerStatus = 0 
 
 burgerButton.onclick = function () {
@@ -12,3 +18,28 @@ burgerButton.onclick = function () {
         burgerStatus = 0 
     }
 }
+// Бургер меню
+
+
+// Обрабатываем событие (click on btnsFriends)
+const openFriendsPopup = () => {
+    friendsWindow.classList.add("open")
+    window.onscroll = () => { window.scroll(0, 0); }
+    if (burgerStatus === 1) {
+        burgerMenu.style.display = "none"
+        burgerStatus = 0
+    }
+}
+
+// Вешаем на классы событие открытия popup-Friends
+btnsFriends.forEach(btn => {
+    btn.addEventListener('click', openFriendsPopup)
+})
+
+// Обработка события клика для закрытия popup-Friends
+friendsWindow.addEventListener("click", (event) => {
+    if (event.target == friendsWindow || event.target == closeBtnfriends) {
+        friendsWindow.classList.remove("open")
+        window.onscroll = () => { window.scroll(); }
+    }
+})
