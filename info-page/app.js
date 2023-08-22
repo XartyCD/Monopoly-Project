78f580ef269b5a911800252d4e96a855a6d999a2
XartyCD <Frexozagsap@yandex.ru>
2023-08-23 00:23:35 +0300

    optimized JS in info-page

--- info-page/app.js
+++ info-page/app.js
@@ -1,33 +1,24 @@
-
 const burgerButton = document.getElementById("burger")
 const burgerMenu = document.getElementById("menu")
 const btnsFriends = document.querySelectorAll(".btn-friends-popup")
 const friendsWindow = document.getElementById("popup-friends")
 
-// Бургер меню (знаю что плохо реализовано (пока и так сойдет))
-let burgerStatus = 0 
-
-
-
+// Бургер меню
 burgerButton.onclick = function () {
-    if (burgerStatus === 0) {
-        burgerMenu.style.display = "block"
-        burgerStatus = 1
+    if (burgerMenu.classList.contains("open")) {
+        burgerMenu.classList.remove("open")
     }
     else {
-        burgerMenu.style.display = "none"
-        burgerStatus = 0 
+        burgerMenu.classList.add("open")
     }
 }
-// Бургер меню
 
 // Обрабатываем событие (click on btnsFriends)
 const openFriendsPopup = () => {
     friendsWindow.classList.add("open")
     window.onscroll = () => { window.scroll(0, 0); }
-    if (burgerStatus === 1) {
-        burgerMenu.style.display = "none"
-        burgerStatus = 0
+    if (burgerMenu.classList.contains("open")) {
+        burgerMenu.classList.remove("open")
     }
 }
 
